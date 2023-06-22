3b16632ff1872bda9f57b95d9af839e7dae6e477
G0dja <91980285+G0dja@users.noreply.github.com>
2023-06-22 07:36:14 +0300

    Delete Diplom directory

--- Diplom/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  user: 'shopdiplom0@gmail.com',
-  clientId: '137458273789-ibaaasgi0dq8e7j4ab9dov9m6oqdjfdn.apps.googleusercontent.com',
-  clientSecret: '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',
-  refreshToken: '1//04-yFx4bufUWHCgYIARAAGAQSNwF-L9IrIZYktAF2HQ9HQGyVEh_E6BbtcH8n2Epze6_mNEbSoCf9hFtDbj4Cxxxyla25vIsDmIQ',
-  website: 'http://ec2-13-48-6-231.eu-north-1.compute.amazonaws.com'
-}
